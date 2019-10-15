3a2aeb1598f9cc234bfde63f40d35a2c32eba902
Kirill Shatskiy <kshatskiy@iponweb.net>
2019-10-15 15:31:48 +0300

    add the second test

--- test/specs/my-first-test.js
+++ test/specs/my-first-test.js
@@ -1,9 +1,21 @@
 const assert = require('assert')
 
+// Course https://learn.webdriver.io/
+// Website https://webdriver.io/
+// API Docs https://webdriver.io/docs/api.html
+
 describe('qa/dev/vjs_vpaid.html', () => {
-    it('should have the right title', async () => {
+    before(async () => {
         await browser.url('https://qa-test.automattic.iponweb.net/qa/dev/vjs_vpaid.html')
+    })
+    it('should have the right title', async () => {
         const title = await browser.getTitle()
         assert.strictEqual(title, 'Automattic Ad Testing Page')
+    })
+
+    it('should have the right header', async () => {
+        const h1 = await $('body h1');
+        const text = await h1.getText();
+        assert.strictEqual(text, 'VJS: VPAID Video')
     })
 })
